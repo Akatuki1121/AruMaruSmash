9ac615bf5530b3d380fc953b1b132810ddd6ba41
kuroda-R <kuroryu5525@gmail.com>
2026-07-21 17:09:03 +0900

    ジョイコンの登録方法を実装中

--- Assets/Scripts/Joycon/JoyCon_Connect_Manager.cs
+++ Assets/Scripts/Joycon/JoyCon_Connect_Manager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 /// <summary>
 /// Joy-Conの接続を管理するクラス。
 /// SL+SRを押した順で登録する。
+/// 誰かの右ボタンで決定する。
 /// スイッチでたまにやるあれのイメージ。
 /// </summary>
 
@@ -10,6 +11,7 @@ public class JoyCon_Connect_Manager : MonoBehaviour
 {
     [Header("Joy-Con設定")]
     public int joyconIndex = 0; // 使用するJoy-Conのインデックス（0または1）
+    const int indexMax = 4;     // Joy-Conの最大接続数
     private Joycon joycon; // 接続されているJoy-Con本体への参照
 
     void Start()
@@ -20,21 +22,31 @@ public class JoyCon_Connect_Manager : MonoBehaviour
     void Update()
     {
         ConnectJoyCon();
+        decisionConnect();
     }
 
     public void ConnectJoyCon()
     {
-        bool isDownSL = false;
-        bool isDownSR = false;
+        bool isHeldSL = false;
+        bool isHeldSR = false;
 
-        isDownSL = joycon.GetButtonDown(Joycon.Button.SL);
-        isDownSR = joycon.GetButtonDown(Joycon.Button.SR);
 
-        if(isDownSL && isDownSR)
+        isHeldSL = joycon.GetButton(Joycon.Button.SL);
+        isHeldSR = joycon.GetButton(Joycon.Button.SR);
+
+        if(isHeldSL && isHeldSR)
         {
             Debug.Log("Joy-Con connected!");
             joycon = JoyconManager.Instance.j[joyconIndex];
-            joyconIndex++; // 次のJoy-Conのインデックスに進める
+            if(joyconIndex < indexMax)
+            {
+                joyconIndex++; // 次のJoy-Conのインデックスに進める
+            }
         }
+    }
+
+    public void decisionConnect()
+    {
+
     }
 }
